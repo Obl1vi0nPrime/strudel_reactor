4b9c9c5b326c830411101f3418ba369adeb1563c
kapny010 <nihalkapadiya04@gmail.com>
2025-11-07 07:37:05 +1030

    Update DJControls.jsx
    UI stylings implemented for DJ controls

--- src/components/DJControls.jsx
+++ src/components/DJControls.jsx
@@ -1,18 +1,21 @@
 function DJControls() {
   return (
       <div>
-          <div className="form-check">
-              <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" defaultChecked />
-              <label className="form-check-label" htmlFor="flexRadioDefault1">
-                  p1: ON
-              </label>
-          </div>
-          
-          <div className="form-check">
-              <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-              <label className="form-check-label" htmlFor="flexRadioDefault2">
-                  p1: HUSH
-              </label>
+          <div className="card shadow-sm mt-3">
+              <div className="card-header py-2">
+                <strong>p1 Track Control</strong>
+              </div>
+              <div className="card-body">
+                  <div className="btn-group w-100" role="group" aria-label="p1 group">
+                    <input type = "radio" className="btn-check" name="flexRadioDefault" id="flexRadioDefault1" defaultChecked></input>
+                      <label className="btn btn-outline-secondary" htmlFor="flexRadioDefault1">
+                        p1: ON
+                      </label>
+                    <input type ="radio" className="btn-check" name="flexRadioDefault" id="flexRadioDefault2"></input>
+                      <label className="btn btn-outline-secondary" htmlFor="flexRadioDefault2"> p1: HUSH </label>
+                  </div>
+
+              </div>
           </div>
 
           {/* Tempo control */}
